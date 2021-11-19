489297a7d2cf30d374ecb9fb0deda71fef237964
chenjiahui <chenjiahui@dossen.com>
2021-11-19 11:22:19 +0800

    移除doc里面的组件，改为使用src里面

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import FormBuilder from '../../src/components/FormBuilder.vue'
+import FormGroup from '../../src/components/FormGroup.vue'
+import FormItem from '../../src/components/FormItem.vue'
+
+Vue.component('FormBuilder', FormBuilder)
+Vue.component('FormGroup', FormGroup)
+Vue.component('FormItem', FormItem)
+
 Vue.use(ElementUI, {
   size: 'small'
 })
